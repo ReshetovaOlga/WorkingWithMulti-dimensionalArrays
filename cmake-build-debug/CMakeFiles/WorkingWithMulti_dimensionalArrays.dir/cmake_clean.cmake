9e1852167e3f21ea20748fe661133e3323025f75
ReshetovaOlga <reshetova_o_v@mail.ru>
2022-02-09 23:40:39 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/WorkingWithMulti_dimensionalArrays.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WorkingWithMulti_dimensionalArrays.dir/main.c.obj"
+  "WorkingWithMulti_dimensionalArrays.exe"
+  "WorkingWithMulti_dimensionalArrays.exe.manifest"
+  "WorkingWithMulti_dimensionalArrays.pdb"
+  "libWorkingWithMulti_dimensionalArrays.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WorkingWithMulti_dimensionalArrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/WorkingWithMulti_dimensionalArrays.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WorkingWithMulti_dimensionalArrays.
+# This may be replaced when dependencies are built.
